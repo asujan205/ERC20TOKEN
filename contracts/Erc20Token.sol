370c8d0bf2578eff80e64530d4911e87bbe5b7e1
Sujan Adhikari <71167057+asujan205@users.noreply.github.com>
2022-04-19 23:24:56 +0545

    INIT

--- contracts/Erc20Token.sol
+++ contracts/Erc20Token.sol
@@ -6,13 +6,54 @@ contract Erc20Token {
     string public symbol ="CRySA";
     string public standard ="CRySA v1.0";
     mapping ( address => uint256 )  public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 //initalize 
     constructor ( ) public {
-        balanceOf[msg.sender]= 100000;
-        totalSupply = 100000;
+        balanceOf[msg.sender]= 10000000;
+        totalSupply = 10000000;
     }
+    event Transfer(
+        address indexed _from ,
+        address indexed _to ,
+        uint256 _value 
+    );
+    event Approve (
+        address indexed _owner ,
+        address indexed  _Spender,
+        uint256 _value
+
+
+    );
+    
 // transfer 
 function transfer( address _to ,uint256 TokenNum) public returns( bool success){
-require(TokenNum <= );
+ require (balanceOf[msg.sender] >= TokenNum);
+ balanceOf[msg.sender]-= TokenNum;
+ balanceOf[_to]+=TokenNum;
+ emit Transfer(msg.sender,_to,TokenNum);
+ return true;
+
 }
+//delegate transfer 
+   function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+
+      emit   Approve(msg.sender, _spender, _value);
+
+        return true;
+    }
+      function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+         emit Transfer(_from, _to, _value);
+
+        return true;
+    }
+
 }
