55cee4757c7bc5a2e21d8f53002e75f5af22665c
Sujan Adhikari <71167057+asujan205@users.noreply.github.com>
2022-04-21 22:00:29 +0545

    settoekn price

--- contracts/ERC20Tokensale.sol
+++ contracts/ERC20Tokensale.sol
@@ -1,8 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+import './Erc20Token.sol';
 contract ERC20Tokensale{
-    function Erc20Tokensale() public {
-        //assign an admin
-        
+    address admin;
+    Erc20Token public tokenContract;
+    uint256 TokenPrice;
+  constructor( Erc20Token _tokenContract, uint256 _tokenPrice) public {
+        //assign admin
+        admin = msg.sender;
+        //tokenContract
+      tokenContract =_tokenContract;
+      //set toekn price in wei
+      TokenPrice = _tokenPrice;
+
+
     }
+    //buy tokens
+    
 }
--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,5 +1,7 @@
 const Erc20Token = artifacts.require("Erc20Token");
-
+const Erc20Tokensale= artifacts.require("ERC20Tokensale")
 module.exports = function(deployer) {
-  deployer.deploy(Erc20Token);
+  deployer.deploy(Erc20Token).then(function(){
+    return deployer.deploy(Erc20Tokensale, Erc20Token.address,10000000000)
+  });
 };
